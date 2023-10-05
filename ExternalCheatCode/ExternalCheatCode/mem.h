cd9c4c6cf0a0c2701c14ee559382a7a3031cd53e
AINS <59402342+ainssssss@users.noreply.github.com>
2023-10-05 18:41:36 +0200

    Delete ExternalCheatCode directory

--- ExternalCheatCode/ExternalCheatCode/mem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "stdafx.h"
-#include <windows.h>
-
-namespace mem
-{
-	void PatchEx(BYTE* dst, BYTE* src, unsigned int size, HANDLE hProcess);
-	void NopEx(BYTE* dst, unsigned int size, HANDLE hProcess);
-}
